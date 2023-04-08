3cca83c93b3b69a75a45f6bb4baf46d4f7e296c9
mszinte <mszinte@invibe.nohost.me>
2023-04-09 00:37:58 +0200

    last changes

--- analysis_code/postproc/prf/webgl/send_index.sh
+++ analysis_code/postproc/prf/webgl/send_index.sh
@@ -1,5 +1,5 @@
 # to run on local
-rsync -avuz --progress ~/disks/meso_S/data/amblyo_prf/derivatives/webgl/sub-01/ admin@invibe.nohost.me:/var/www/my_webapp__5/www/sub-01/
+rsync -avuz --progress ~/disks/meso_S/data/amblyo_prf/derivatives/webgl/ admin@invibe.nohost.me:/var/www/my_webapp__5/www/
 
 # to run on invibe with root user
 chmod -Rfv 777 /var/www/my_webapp__5/
