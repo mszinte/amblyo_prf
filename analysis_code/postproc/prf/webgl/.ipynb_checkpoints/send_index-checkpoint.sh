832da7549773a553dfd7bc3caee71a0f78a485ec
mszinte <mszinte@invibe.nohost.me>
2023-04-09 09:49:41 +0200

    changes about sending of the dataset to webgl app

--- analysis_code/postproc/prf/webgl/.ipynb_checkpoints/send_index-checkpoint.sh
+++
@@ -1,5 +0,0 @@
-# to run on local
-rsync -avuz --progress ~/disks/meso_S/data/amblyo_prf/derivatives/webgl/ admin@invibe.nohost.me:/var/www/my_webapp__5/www/
-
-# to run on invibe with root user
-chmod -Rfv 777 /var/www/my_webapp__5/
